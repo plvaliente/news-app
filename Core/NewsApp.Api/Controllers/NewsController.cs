efe3188e98c0003bedd09e84a452ea331abf88fc
Patricio Lopez Valiente <patriciolopezvaliente@gmail.com>
2023-06-26 19:51:29 -0300

    feat: -config inicial projects newsapiservice -config automapper -config inicial template controller news

---
+++ Core/NewsApp.Api/Controllers/NewsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NewsApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NewsController : ControllerBase
+    {
+        private readonly ILogger<NewsController> _logger;
+
+        public NewsController(ILogger<NewsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("search")]
+        public ActionResult<object> Search(DateTime? dateFrom, DateTime? dateTo, string[] keywords, int page, int pageSize)
+        {
+            try
+            {
+                if (keywords.Length == 0 || page == 0 || pageSize == 0) return BadRequest(new object());
+
+                return Ok(new object()); ;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError, new object());
+            }
+        }
+
+        [Route("top-headlines")]
+        public ActionResult<object> HeadLines(string country, int page, int pageSize)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(country) || page == 0 || pageSize == 0) return BadRequest(new object());
+
+                return Ok(new object()); ;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError, new object());
+            }
+        }
+    }
+}
---
+++ Core/NewsApp.External.Services/NewsApiService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NewsAPI;
+using System;
+
+namespace NewsApp.External.Services
+{
+    public class NewsApiService
+    {
+        private string _apiKey;
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+        private readonly NewsApiClient _client;
+
+        public NewsApiService(IConfiguration conf, ILogger logger)
+        {
+            _configuration = conf;
+            _logger = logger;
+            _apiKey = _configuration.GetSection("ExternalNewsApiKey").Value!;
+            _client = new NewsApiClient(_apiKey);
+        }
+    }
+}
