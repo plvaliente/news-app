afe2e71412d9b46e0c357097e4acda52106490c4
Patricio Lopez Valiente <patriciolopezvaliente@gmail.com>
2023-06-26 20:17:22 -0300

    feat: - projs services y crossapps - dtos news y responses

---
+++ Core/NewsApp.CrossCuttingApp/Dto/NewsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NewsApp.CrossCuttingApp.Dto
+{
+    public class NewsDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Source { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
